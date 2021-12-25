00e6871bfa8fefd46f1abbec6a1071a0e252841c
Yann Mazita <dev.mazita@gmail.com>
2021-12-25 01:01:33 +0100

    Fixed comments

--- include/calc/calculator.h
+++ include/calc/calculator.h
@@ -35,11 +35,11 @@ namespace calc{
             * @return true if target found in tree else false.
             */
             int Find(std::string target, Node* &node);
+            // A reference to a pointer is used so as to keep
+            // modifications of the pointer.
 
             /**
              * Allocate memory for new node.
-             * A reference to a pointer is used so as to keep
-             * modifications of the pointer.
              * @param newData the data to be stored in the node.
              * @return pointer to the the new node.
              */
@@ -51,13 +51,13 @@ namespace calc{
              * first empty child is found. Then when it's found,
              * store in it the wanted data using NewNode(). If no
              * node is given, the root node will be used.
-             * A reference to a pointer is used so as to keep
-             * modifications of the pointer.
              * @param newData the data to be stored in the node.
              * @param node the starting node.
              * @return pointer to the updated node.
              */
             void Insert(std::string newData, Node* &node);
+            // A reference to a pointer is used so as to keep
+            // modifications of the pointer.
 
         public:
             int IsTargetInTree(std::string target);
