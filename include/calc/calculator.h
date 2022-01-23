d6df06f6d457608296286eda72d2d2d01017918d
Yann Mazita <dev.mazita@gmail.com>
2022-01-23 06:55:08 +0100

    Removed unused methods

--- include/calc/calculator.h
+++ include/calc/calculator.h
@@ -102,16 +102,6 @@ namespace calc{
             BinaryTree(std::string inputExpr);
         private:
             /**
-            * Find target in (sub)tree.
-            * @param target target to find in tree.
-            * @param node the starting node for the recursion.
-            * @return true if target found in tree else false.
-            */
-            int Find(std::string target, Node* &node);
-            // A reference to a pointer is used so as to keep
-            // modifications of the pointer.
-
-            /**
              * Allocate memory for new node.
              * @param newData the data to be stored in the node.
              * @return pointer to the the new node.
@@ -119,25 +109,8 @@ namespace calc{
             Node* NewNode(std::string newData);
 
             /**
-             * Insert new node with data.
-             * Recursively go through the nodes of a tree until the
-             * first empty child is found. Then when it's found,
-             * store in it the wanted data using NewNode(). If no
-             * node is given, the root node will be used.
-             * @param newData the data to be stored in the node.
-             * @param node the starting node.
-             * @return pointer to the updated node.
-             */
-            void Insert(std::string newData, Node* &node);
-            // A reference to a pointer is used so as to keep
-            // modifications of the pointer.
-
-            /**
              * Populate the binary tree from the postfix expression.
              */
             void Populate();
-        public:
-            int IsTargetInTree(std::string target);
-            void InsertTarget(std::string newData);
     };
 }
--- lib/calc/calculator.cpp
+++ lib/calc/calculator.cpp
@@ -186,55 +186,12 @@ namespace calc{
         expr = new Expression (inputExpr);
     }
 
-    int BinaryTree::Find(std::string target, Node* &node){
-        if (node == NULL){
-            return false;
-        }
-        else if (node->data == target){
-                return true;
-        }
-        if (node->right != NULL){
-            return BinaryTree::Find(target, node->right);
-        }
-        if (root->left != NULL){
-            return BinaryTree::Find(target, node->left);
-        }
-        return -1;
-    }
-
     Node* BinaryTree::NewNode(std::string newData){
         Node* node = new Node;
         node->data = newData;
         node->left = NULL;
         node->right = NULL;
         return node;
-    }
-
-    void BinaryTree::Insert(std::string newData, Node* &node){
-        // node is empty (NULL pointer)
-        if (node == NULL){
-            node = BinaryTree::NewNode(newData);
-        }
-        // Left child is empty
-        else if (node->left == NULL){
-            BinaryTree::Insert(newData, node->left);
-        }
-        // Right child is empty
-        else if (node->right == NULL){
-            BinaryTree::Insert(newData, node->right);
-        }
-        // 
-        else{
-            BinaryTree::Insert(newData, node->right);
-        }
-    }
-    // Public acces methods
-    int BinaryTree::IsTargetInTree(std::string target){
-        return BinaryTree::Find(target, root);
-    }
-
-    void BinaryTree::InsertTarget(std::string newData){
-        BinaryTree::Insert(newData, root);
     }
 
     void BinaryTree::Populate(){
