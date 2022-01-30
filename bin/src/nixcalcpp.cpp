400c69fea17c079127d4324c43050cdf20926948
Yann Mazita <dev.mazita@gmail.com>
2022-01-30 16:38:29 +0100

    Added command line interaction

--- bin/src/nixcalcpp.cpp
+++ bin/src/nixcalcpp.cpp
@@ -6,18 +6,7 @@ int main(int argc, char* argv[])
 {
     std::ios_base::sync_with_stdio(false);
 
-    /*
-    std::string someString = "1+2.5*(-3.5)*2*(-3+2)+2^(5-1)";
-    calc::BinaryTree btree;
-    btree.InsertTarget(std::to_string('+'));
-    btree.InsertTarget(std::to_string('1'));
-    btree.InsertTarget(std::to_string('+'));
-    btree.InsertTarget(std::to_string('3'));
-    btree.IsTargetInTree(std::to_string('3'));
-    */
-
-    calc::Expression newExpr("3+4*2/(1-5)^2^3");
-    newExpr.DisplayPostfix();
-
+    calc::BinaryTree expression(argv[1]);
+    expression.ComputeAndDisplay();
     return 0;
 }
